2d8ab66d4b388f1bd9281661a3b450fcb1f02c3d
Mide001 <olamideisreal5@gmail.com>
2023-10-04 11:35:50 +0100

    reduced font

--- src/components/Verified.js
+++ src/components/Verified.js
@@ -8,28 +8,28 @@ const Verified = () => {
   return (
     <div className="flex justify-center mt-16">
       <div className="w-1/4 p-4 text-center">
-        <div className="text-4xl mb-2 h-16 w-16 border border-gray-900 rounded-full flex items-center justify-center mx-auto">
+      <div className="text-4xl mb-2 h-16 w-16 border border-gray-900 rounded-full flex items-center justify-center mx-auto">
           <img src={secure} alt="kyc icon" />
         </div>
-        <p className="mt-2 text-[#203475]">Team KYC</p>
+        <p className="mt-2 text-[#203475] text-xs md:text-ssm">Team KYC</p>
       </div>
       <div className="w-1/4 p-4 text-center">
         <div className="text-4xl mb-2 h-16 w-16 border border-gray-900 rounded-full flex items-center justify-center mx-auto">
           <img src={web} alt="web icon" />
         </div>
-        <p className="mt-2 text-[#203475]">Websites and Apps</p>
+        <p className="mt-2 text-[#203475] text-xs md:text-ssm">Websites and Apps</p>
       </div>
       <div className="w-1/4 p-4 text-center">
         <div className="text-4xl mb-2 h-16 w-16 border border-gray-900 rounded-full flex items-center justify-center mx-auto">
           <img src={contract} alt="contract icon" />
         </div>
-        <p className="mt-2 text-[#203475]">Smart Contract Audit</p>
+        <p className="mt-2 text-[#203475] text-xs md:text-ssm">Smart Contract Audit</p>
       </div>
       <div className="w-1/4 p-4 text-center">
         <div className="text-4xl mb-2 h-16 w-16 border border-gray-900 rounded-full flex items-center justify-center mx-auto">
           <img src={money} alt="money-1 icon" />
         </div>
-        <p className="mt-2 text-[#203475]">Bounty Program</p>
+        <p className="mt-2 text-[#203475] text-xs md:text-ssm">Bounty Program</p>
       </div>
     </div>
   );
