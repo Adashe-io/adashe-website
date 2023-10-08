315203e880d92e3795cde10ef615861bd9240aa3
Mide001 <olamideisreal5@gmail.com>
2023-10-08 13:52:59 +0100

    fixed readMore

--- src/components/FeatureComponent.js
+++ src/components/FeatureComponent.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import moneyExchange from "../assets/icons/money-exchange.png";
 import moneyLogo from "../assets/icons/money.png";
 import investmentLogo from "../assets/icons/investment.png";
 import databaseLogo from "../assets/icons/database-usage.png";
 
 const FeatureComponent = () => {
+  const initialText = `Adashe is a seamless fusion of Tradefi, DeFi, and Data systems, It is the future of Money, It is available across multiple Blockchains. Developed by crypto pioneers, aerospace inventors, engineers, data scientist and tech leaders, Adashe establishes a standardized industry framework for trade, finance, data and for adoption of blockchain and crypto.Adabase is a federated data solution which will allow for robust and real time interactions of and with data from an endless stream of sources. Supply chains, performance data tracking and automated execution of commands specific to user and situation are all uses, giving users a detailed snapshot and the ability to respond faster and more precisely. Our crypto currency lubricates the ecosystem and serves as payment for interactions at predetermined intervals. Enterprise and non-enterprise users can interact with our Eco-system via our app and web-based solutions across different platforms for free. Adabase while built to work with Adashe can via smart contracts execute payments for other chains with associated fees, furthermore Project Adashe plans to eventually fork the Polygon chain for it custom blockchain protocol.`;
+
+  const [showFullText, setShowFullText] = useState(false);
+
+  const toggleText = () => {
+    setShowFullText(!showFullText);
+  };
+
   return (
     <div className="container mx-auto py-8">
       <div className="p-5 rounded-lg">
-        <h2 className="text-xl font-semibold text-[#203475] mb-4">Project Adashe</h2>
+        <h2 className="text-xl font-semibold text-[#203475] mb-4">
+          Project Adashe
+        </h2>
         <p className="text-lg font-light mb-6">
-          Adashe is a seamless fusion of Tradefi, DeFi, and Data systems. It is
-          the future of money, available across multiple blockchains. Developed
-          by crypto pioneers, aerospace inventors, engineers, data scientists,
-          and tech leaders, Adashe establishes a standardized industry framework
-          for trade, finance, data, and the adoption of blockchain and
-          cryptocurrency...view more
+          {showFullText ? initialText : initialText.slice(0, 351)}
+          <span onClick={toggleText} className="cursor-pointer underline">
+            {showFullText ? "view less" : "view more"}
+          </span>
         </p>
       </div>
       <div className="bg-[#203475] p-4 rounded-lg mt-6 featureComponentContainer">
